0ee9593dea37ac7e62be3e246a362c9a9d84b029
DevonCarswell <kaszazsolt90@gmail.com>
2022-06-01 13:21:10 +0200

    create map1

---
+++ Assets/Source/Actors/Librarian/Bed.cs
@@ -0,0 +1,20 @@
+﻿using DungeonCrawl.Actors.Characters;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class  Bed : Actor
+    {
+        public override int DefaultSpriteId => 388;
+        public override string DefaultName => "Bed";
+        public override int Z => -1;
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor is Ghost)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Source/Actors/Librarian/Table1.cs
@@ -0,0 +1,20 @@
+﻿using DungeonCrawl.Actors.Characters;
+
+namespace DungeonCrawl.Actors.Static
+{
+    public class  Table1 : Actor
+    {
+        public override int DefaultSpriteId => 337;
+        public override string DefaultName => "Table1";
+        public override int Z => -1;
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor is Ghost)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
